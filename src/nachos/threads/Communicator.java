b4b8315f9b3be058ac0395bb2999eda884e5b883
Joshua Reno <jralphur@gmail.com>
2024-03-06 19:19:48 -0800

    communicator, fails to pass second test

--- src/nachos/threads/Communicator.java
+++ src/nachos/threads/Communicator.java
@@ -10,10 +10,19 @@ import nachos.machine.*;
  * threads can be paired off at this point.
  */
 public class Communicator {
+    private final Condition condition;
+    private final Lock lock;
+    private int balance;
+    private Integer buf;
+
     /**
      * Allocate a new communicator.
      */
     public Communicator() {
+        this.lock = new Lock();
+        this.condition = new Condition(lock);
+        this.buf = null;
+        this.balance = 0;
     }
 
     /**
@@ -27,6 +36,20 @@ public class Communicator {
      * @param	word	the integer to transfer.
      */
     public void speak(int word) {
+        this.lock.acquire();
+
+        if (this.buf != null) { // another speaker is waiting to be paired
+            this.condition.sleep();
+        }
+
+        this.buf = word;
+
+        if (this.balance > 0) { // a listener was waiting for us
+            this.condition.wake();
+        }
+
+        this.condition.sleep(); // wait for listener, finished with a listener once we return
+        this.lock.release();
     }
 
     /**
@@ -36,6 +59,21 @@ public class Communicator {
      * @return	the integer transferred.
      */    
     public int listen() {
-	return 0;
+        this.lock.acquire();
+
+        if (this.buf == null) {
+            if (this.balance < 0) {
+                this.condition.wake();
+            }
+            this.balance++;
+            this.condition.sleep();
+        }
+
+        int ret = this.buf;
+        this.buf = null;
+        this.condition.wake();
+        this.lock.release();
+
+        return ret;
     }
 }
